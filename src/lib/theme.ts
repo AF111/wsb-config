07a77b10f17c57c5a8efacea9fd884a7f08bec9a
AF111 <30418824+AF111@users.noreply.github.com>
2021-12-16 18:17:33 +0100

    fix theme changing

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -4,42 +4,51 @@ export type Theme = 'system' | 'dark' | 'light';
 
 const themes = ['system', 'light', 'dark'];
 
+/**
+ * @description Gets the persisted theme
+ * @returns {Theme}
+ */
 export const getCurrentTheme = (): Theme => {
-	let persistedTheme = localStorage.getItem('app-theme') as Theme | null;
-
-	if (persistedTheme) {
-		if (!themes.includes(persistedTheme)) {
-			return darkModeMQ.matches ? 'dark' : 'light';
-		}
-		return persistedTheme;
-	}
-
-	return darkModeMQ.matches ? 'dark' : 'light';
+    return (localStorage.getItem('app-theme') as Theme) ?? 'system';
 };
 
-const logTheme = console.log.bind(console, '🌟 Theme changed to:');
+/**
+ * @description Returns theme style: dark if supported or light
+ * @returns {('dark' | 'light')}
+ */
+export const getThemeStyle = (): Exclude<Theme, 'system'> => {
+    const currentTheme = getCurrentTheme();
 
-export const changeTheme = (theme?: Theme) => {
-	if (!supportsDarkMode || !themes.includes(theme)) return;
+    switch (currentTheme) {
+        case 'dark':
+            return supportsDarkMode ? 'dark' : 'light';
+        case 'light':
+            return 'light';
 
-	if (theme === 'system') {
-		if (darkModeMQ.matches) {
-			document.body.classList.add('themed--dark');
-			logTheme('system default (dark)');
-		} else {
-			document.body.classList.remove('themed--dark');
-			logTheme('system default (light)');
-		}
-		localStorage.setItem('app-theme', theme);
-		return;
-	}
+        default:
+            return supportsDarkMode && darkModeMQ.matches ? 'dark' : 'light';
+    }
+};
 
-	localStorage.setItem('app-theme', theme);
-	if (theme === 'dark') {
-		document.body.classList.add('themed--dark');
-		logTheme('dark');
-	} else {
-		document.body.classList.remove('themed--dark');
-		logTheme('light');
-	}
+const logTheme = (sys: boolean, ...args: string[]) => {
+    console.log('🌟 Theme changed to:', ...args, sys ? '(system)' : '(user)');
+};
+
+/**
+ * @param {Theme} theme to change to
+ * @returns {void}
+ */
+export const changeTheme = (theme?: Theme): void => {
+    if (!supportsDarkMode || !themes.includes(theme)) return;
+
+    localStorage.setItem('app-theme', theme);
+    const themeStyle = getThemeStyle();
+
+    if (themeStyle === 'dark') {
+        document.body.classList.add('themed--dark');
+        logTheme(theme === 'system', 'dark');
+    } else {
+        document.body.classList.remove('themed--dark');
+        logTheme(theme === 'system', 'light');
+    }
 };
